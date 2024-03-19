e04bd75dd1dde2c1dd8864d2bc9477340568362b
Stefan Maftei <115487392+luvmafi@users.noreply.github.com>
2024-03-19 15:03:47 +0200

    lab4

--- lab4/src/Application.java
+++ lab4/src/Application.java
@@ -4,7 +4,7 @@ import Shapes.Patrulater;
 public class Application {
     public static void main(String[] args) {
         Cerc cerc = new Cerc(5);
-        Patrulater patrulater = new Patrulater(10);
+        Patrulater patrulater = new Patrulater(10,11,12,13);
 
         System.out.println("Perimetrul cercului este: " + cerc.calculPerimetru());
         System.out.println("Perimetrul patrulaterului este: " + patrulater.calculPerimetru());
--- lab4/src/Shapes/Patrulater.java
+++ lab4/src/Shapes/Patrulater.java
@@ -3,14 +3,17 @@ package Shapes;
 import Interface.Calcul;
 
 public class Patrulater implements Calcul {
-    private double latura;
+    private double latura1, latura2, latura3, latura4;
 
-    public Patrulater(double latura) {
-        this.latura = latura;
+    public Patrulater(double latura1, double latura2, double latura3, double latura4) {
+        this.latura1 = latura1;
+        this.latura2 = latura2;
+        this.latura3 = latura3;
+        this.latura4 = latura4;
     }
 
     @Override
     public double calculPerimetru() {
-        return 4 * latura;
+        return latura1 + latura2 + latura3 + latura4;
     }
 }
