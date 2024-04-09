c510164e3c3f1fbe56ff5f155e897359c2ae12b1
Ștefan Maftei <115487392+luvmafi@users.noreply.github.com>
2024-04-09 10:53:56 -0700

    Delete proiectPAO/untitled directory

--- proiectPAO/untitled/src/dao/RetetaDao.java
+++
@@ -1,9 +0,0 @@
-package dao;
-import model.Reteta;
-
-public interface RetetaDao {
-    void adaugaReteta(Reteta reteta);
-    Reteta obtineRetetaDupaId(String id);
-    void actualizeazaReteta(Reteta reteta);
-    void stergeRetetaDupaId(String id);
-}
