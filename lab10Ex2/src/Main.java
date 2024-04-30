db500b69d97fb11223f489bd5df0e0ff3018ddc8
Ștefan Maftei <stefan.maftei1@s.unibuc.ro>
2024-04-30 15:03:30 +0300

    Update Main.java

--- lab10Ex2/src/Main.java
+++ lab10Ex2/src/Main.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String[] args) {
     int[] ar2 = {4,1,3,2,16,9,10,14,8,7};
     Arrays.sort(ar2);
-    System.out.println(Arrays.toString(ar2));
+    System.out.println("Array-ul sortat crescator: " + Arrays.toString(ar2));
     int[] sortedDesc = Arrays.stream(ar2).boxed().sorted(Collections.reverseOrder()).mapToInt(Integer::intValue).toArray();
-    System.out.println(Arrays.toString(sortedDesc));
+    System.out.println("Array-ul sortat descrescator: " + Arrays.toString(sortedDesc));
     int[] squared = Arrays.stream(ar2).map(x -> x * x).toArray();
-    System.out.println(Arrays.toString(squared));
+    System.out.println("Array-ul cu elementele ridicate la patrat: " + Arrays.toString(squared));
     }
 }
